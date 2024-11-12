e756d80d4c2943618e2496165c563b3373a2f293
Adrien Brunet <64930588+adrien-may@users.noreply.github.com>
2024-11-12 19:49:29 +0100

    🤩 First templates

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>ECM Bonjour</h1>'
+    @app.route('/')
+    def index():
+        return '<h1>ECM Bonjour</h1>'
 
 
-@app.route('/user/<name>')
-def user(name):
-    return f'<h1>Hello, {name}</h1>'
+    @app.route('/user/<name>')
+    def user(name):
+        return render_template('user.html', name=name)
+
+    return app
