80023df02297ce5e8d953355d3f4b78784c40756
Adrien Brunet <64930588+adrien-may@users.noreply.github.com>
2024-11-12 20:06:43 +0100

    😵 first_db

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+import os
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy(model_class=Base)
+
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data.sqlite')}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from tasks.models import Task
+
+    Migrate(app, db)
 
     @app.route('/')
     def index():
--- conftest.py
+++ conftest.py
@@ -1,17 +1,23 @@
 import pytest
-
 from flask import template_rendered
-
-from app import create_app
+from app import create_app, db as _db
+from tests import common
 
 @pytest.fixture
 def user_name():
     return "adrien"
 
 
-@pytest.fixture
-def app():
+@pytest.fixture(scope='session')
+def app(request):
     app = create_app()
+    # Establish an application context before running the tests.
+    ctx = app.app_context()
+    ctx.push()
+    def teardown():
+        ctx.pop()
+
+    request.addfinalizer(teardown)
     return app
 
 
@@ -27,3 +33,27 @@ def captured_templates(app):
         yield recorded
     finally:
         template_rendered.disconnect(record, app)
+
+@pytest.fixture(scope="session")
+def db(app, request):
+    def teardown():
+        _db.drop_all()
+
+    _db.app = app
+    _db.create_all()
+    request.addfinalizer(teardown)
+    return _db
+
+@pytest.fixture(scope="function")
+def session(db, request):
+    connection = db.engine.connect()
+    transaction = connection.begin()
+    session = common.Session(bind=connection)
+    db.session = common.Session
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        common.Session.remove()
+    request.addfinalizer(teardown)
+    return session
